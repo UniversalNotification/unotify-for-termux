205ee22adca1be0ff44e96a61710ffe1362fbcff
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-01-09 23:36:51 +0800

    feat: remove termux module

--- src/temux.ts
+++ src/temux.ts
@@ -1,28 +1,29 @@
-/* @ts-ignore */
-import * as termux from 'termux'
 import * as fs from 'fs-extra'
 import { UniversalNotification } from '@utils/parse-universal-notification'
 import { downloadImage } from '@utils/download-image'
 import { getResultPromise } from 'return-style'
+import { exec } from 'child_process'
+import { parallel } from 'extra-promise'
 
 export async function notify(notification: UniversalNotification): Promise<void> {
+  const clean: Array<() => Promise<void>> = []
+  const options: string[] = []
+  if (notification.title) options.push('--title', escape(notification.title))
+  if (notification.message) options.push('--content', escape(notification.message))
+  if (notification.url) options.push('--action', escape(`termux-open-url ${escape(notification.url)}`))
   if (notification.imageUrl) {
     const imageFilename = await getResultPromise(downloadImage(notification.imageUrl))
-    try {
-      await termux.notification()
-        .title(notification.title)
-        .content(notification.message)
-        .url(notification.url)
-        .image(imageFilename)
-        .run()
-    } finally {
-      if (imageFilename) await fs.remove(imageFilename)
+    if (imageFilename) {
+      options.push('--image-path', escape(imageFilename))
+      clean.push(() => fs.remove(imageFilename))
     }
-  } else {
-    await termux.notification()
-      .title(notification.title)
-      .content(notification.message)
-      .url(notification.url)
-      .run()
   }
+  exec(`termux-notification ${options.join(' ')}`)
+  await parallel(clean)
+}
+
+function escape(str: string): string {
+  const result = str.replace(/\\/g, '\\\\')
+                    .replace(/'/g, "\\'")
+  return `'${result}'`
 }
