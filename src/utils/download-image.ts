3ab30a7619c54faa7811e735a01cf1bf9310a39f
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-05-08 08:39:55 +0000

    refactor: replace tmp-promise with extra-filesystem

--- src/utils/download-image.ts
+++ src/utils/download-image.ts
@@ -1,7 +1,7 @@
-import { file } from 'tmp-promise'
-import { createWriteStream } from 'fs'
+import { createWriteStream, promises as fs } from 'fs'
 import { pipeline } from 'stream'
 import { promisify } from 'extra-promise'
+import { createTempFile } from 'extra-filesystem'
 import getUri = require('get-uri')
 const getURI = promisify<NodeJS.ReadableStream>(getUri)
 
@@ -9,16 +9,16 @@ export function downloadImage(uri: string): Promise<string> {
   return new Promise<string>(async (resolve, reject) => {
     try {
       const stream = await getURI(uri)
-      const { path, cleanup } = await file()
+      const filename = await createTempFile()
       pipeline(
         stream
-      , createWriteStream(path)
+      , createWriteStream(filename)
       , async err => {
           if (err) {
-            await cleanup()
+            await fs.rm(filename)
             reject()
           } else {
-            resolve(path)
+            resolve(filename)
           }
         }
       )
